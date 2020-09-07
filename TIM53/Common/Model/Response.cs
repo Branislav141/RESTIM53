21205c13411ef923b4bf4d8b1ac9d42ff697feaf
Branislav141 <branislavperovic3@gmail.com>
2020-09-07 17:48:58 +0200

    Zavrsen model podataka.

---
+++ TIM53/Common/Model/Response.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Model
+{
+    public enum STATUS_CODES
+    {
+        REJECTED = 3000,
+        BAD_FORMAT = 5000,
+        SUCCESS = 2000
+    }
+    public class Odgovor
+    {
+        public string Status { get; set; }
+        public STATUS_CODES StatusCode { get; set; }
+        public object Payload { get; set; }
+
+        public Odgovor(STATUS_CODES code, object payload)
+        {
+            Status = code.ToString();
+            StatusCode = code;
+            payload = payload;
+        }
+    }
+}
---
+++ TIM53/Common/Model/Zahtev.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Model
+{
+    public class Zahtev
+    {
+        public string Verb { get; set; }
+        public string Noun { get; set; }
+        public string Query { get; set; }
+        public string Fields { get; set; }
+
+
+        public Zahtev()
+        {
+
+        }
+    }
+}
