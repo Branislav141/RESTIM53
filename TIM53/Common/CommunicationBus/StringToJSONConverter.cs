09ddeb7867322ec01d1964321761b41d05f30f22
stevaa6 <64158795+stevaa6@users.noreply.github.com>
2020-09-09 01:42:16 +0200

    Model update.

--- TIM53/Common/CommunicationBus/StringToJSONConverter.cs
+++ TIM53/Common/CommunicationBus/StringToJSONConverter.cs
@@ -15,29 +15,58 @@ namespace Common.CommunicationBus
         }
         public Zahtev Convert(string stringRequest)
         {
+            Zahtev zahtev = new Zahtev();
             string[] delovi = stringRequest.Split('/');
+
+            zahtev.Verb = delovi[0];
+            
             if (delovi[0].ToUpper() == "GET")
             {
+                //GET/Resurs
+                if (delovi.Length == 2) 
+                { 
+                    zahtev.Noun = "/" + delovi[1]; 
+                }
+                //get/Resurs/1
+                else if (delovi.Length >= 2) 
+                { 
+                    zahtev.Noun = "/" + delovi[1] + "/" + delovi[2]; 
+                }
+                //get/Resurs/1/name='pera';type=1/
+                if (delovi.Length >= 4) 
+                { 
+                    zahtev.Query = delovi[3]; 
+                }
+                //get/Resurs/1/name='pera';type=1/id;name;surname
+                if (delovi.Length == 5)
+                {
+                    zahtev.Fields = delovi[4];
+                }
             }
             else if (delovi[0].ToUpper() == "POST")
             {
+                zahtev.Noun = "/" + delovi[1];
+                zahtev.Query = delovi[2];
             }
             else if (delovi[0].ToUpper() == "PATCH")
             {
+                zahtev.Noun = "/" + delovi[1] + "/" + delovi[2];
+                if (delovi.Length == 5) 
+                { 
+                    zahtev.Query = delovi[4]; 
+                }
+                zahtev.Fields = delovi[3];
             }
             else if (delovi[0].ToUpper() == "DELETE")
             {
+                zahtev.Noun = "/" + delovi[1] + "/" + delovi[2];
             }
             else
             {
                 return null;
             }
 
-            Zahtev request = new Zahtev();
-            request.Verb = delovi[0];
-            request.Noun = delovi[1];
-
-            return request;
+            return zahtev;
         }
     }
 }
--- TIM53/Common/Model/Zahtev.cs
+++ TIM53/Common/Model/Zahtev.cs
@@ -13,6 +13,22 @@ namespace Common.Model
         public string Query { get; set; }
         public string Fields { get; set; }
 
+        public Zahtev(string verb, string noun, string query, string fields)
+        {
+            Verb = verb;
+            Noun = noun;
+            Query = query;
+            Fields = fields;
+        }
+
+        public override string ToString()
+        {
+            return "Verb: " + Verb + System.Environment.NewLine
+                + "Noun: " + Noun + System.Environment.NewLine
+                + "Query: " + Query + System.Environment.NewLine
+                + "Fields: " + Fields + System.Environment.NewLine;
+
+        }
 
         public Zahtev()
         {
